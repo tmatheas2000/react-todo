626712f2504259e6769fa1706339b85a53e9accd
Matheas T <matheas.t@thoughtworks.com>
2023-08-10 11:57:56 +0530

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {createRoot} from 'react-dom';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
 
-const rootElement = document.getElementById('root');
-const root = createRoot(rootElement);
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById('root')
+);
+
